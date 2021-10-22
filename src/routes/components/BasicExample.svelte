17e15035f95ae289a1ad75132fa1959f15ea0184
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-10-23 03:32:43 +0900

    Add test files.

---
+++ src/routes/components/BasicExample.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { Options } from '@splidejs/splide';
+  import { Splide, SplideSlide } from '$lib/components';
+  import { generateSlides } from '../utils';
+
+  const slides = generateSlides();
+  const options: Options = {
+    rewind : true,
+    perPage: 2,
+    gap    : '1rem',
+    height : '15rem',
+    cover  : true,
+    breakpoints: {
+      1000: {
+        perPage: 1,
+      },
+    },
+  }
+</script>
+
+<div class="wrapper">
+	<h2>Basic</h2>
+
+	<Splide
+		options={ options }
+		on:mounted={ e => console.log( e.detail.splide.length ) }
+		on:move={ e => console.log( 'move to', e.detail.index ) }
+	>
+		{ #each slides as slide }
+			<SplideSlide>
+				<img src={ slide.src } alt={ slide.alt }>
+			</SplideSlide>
+		{ /each }
+	</Splide>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
+  import BasicExample from './components/BasicExample.svelte';
+</script>
+
+<main>
+	<BasicExample />
+</main>
