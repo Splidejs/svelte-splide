92ebe147b966abcd250fe54b8ed089a2067ca02b
NaotoshiFujita <fujita.naotoshi@gmail.com>
2021-10-23 09:16:32 +0900

    Optimize the demo page.

--- examples/svelte.config.js
+++ examples/svelte.config.js
@@ -1,15 +1,21 @@
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    floc   : true,
+    appDir : 'app',
+    target : '#svelte',
+    adapter: adapter(),
+    paths  : {
+      base: dev ? '' : '/svelte-splide',
+    }
+  },
 };
 
 export default config;
